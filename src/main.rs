a395258272e17548aa9e423d329a1e52e9b1376d
sasanzare <sasanzare75@gmail.com>
2022-08-03 22:43:06 +0430

    Complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please Enter Your Weight (KG)");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    println!("weight on Mars: {} KG ", calculate_weight_on_mars(input.trim().parse().unwrap()));
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
 }
